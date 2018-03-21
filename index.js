cfb583bb1452d17f83db394b19f38c2bbca0afa7
Ari Simon <arisimon2@gmail.com>
2018-03-20 20:47:04 -0700

    updated apis
    updated APIs for testing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-/*const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
+const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
 
-
-$.ajax({
+function getHospitalList() {
+	$.ajax({
     url: HOSPITAL_URL,
     type: 'GET',
     data: {
@@ -13,14 +13,13 @@ $.ajax({
   alert("Retrieved " + data.length + " records from the dataset!");
   console.log(data);
 
-}); */
+});
+}
 
-/*const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM&callback=initMap';
-const GOOGLE_MAPS_KEY = 'AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM'; */
 
 const DOCTOR_URL = 'https://npiregistry.cms.hhs.gov/api/';
 
-function getDoctorList(number, first_name, last_name, callback) {
+function getDoctorList(data, callback) {
   const settings = {
     url: DOCTOR_URL,
     data: {
@@ -40,4 +39,18 @@ function getDoctorList(number, first_name, last_name, callback) {
 
 
 
+/*const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM&callback=initMap';
+const GOOGLE_MAPS_KEY = 'AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM'; 
 
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(event) {
+    event.preventDefault();
+    let query = $(this).find('.js-search-input').val();
+    getDoctorList();
+
+
+  });
+}
+
+watchSubmit(); */
