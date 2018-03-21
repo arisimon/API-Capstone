ad831f32688ab0bda97ca6fda0f1618bc2c6b7dd
Ari Simon <arisimon2@gmail.com>
2018-03-21 16:52:27 -0700

    updated function, got Hospital API to work

--- index.js
+++ index.js
@@ -3,31 +3,34 @@
 const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
 
 function getHospitalList() {
-	$.ajax({
-    url: HOSPITAL_URL,
-    type: "GET",
-    data: {
-      "$limit" : 25,
-      city: `${city}`,
-    }
-}).done(function(data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-});
-}
+    $.ajax({
+        url: HOSPITAL_URL,
+        type: "GET",
+        dataType: "json",
+        data: {
+            "$limit": 10,
+        }
 
-getHospitalList();
 
+    }).done(function(data) {
+        alert("Retrieved " + data.length + " records from the dataset!");
+        console.log(data);
+        $.each(data, function(index, city) {
+        	$('.hospital-list').html(
+        		`THE ` + data[index].hospital_name + ` IS LOCATED IN ` + data[index].city
+        )});
+
+    });
+
+};
 
 /*const DOCTOR_URL = 'https://npiregistry.cms.hhs.gov/api/';
 
-function getDoctorList(data, callback) {
+function getDoctorList() {
   const settings = {
     url: DOCTOR_URL,
     data: {
-        number,
-        first_name,
-        last_name
+        "$limit": 15,
     },
     dataType: 'json',
     type: 'GET',
@@ -38,22 +41,22 @@ function getDoctorList(data, callback) {
 
   $.ajax(settings);
 }
+*/
 
 
 
 /*const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM&callback=initMap';
-const GOOGLE_MAPS_KEY = 'AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM'; 
+const GOOGLE_MAPS_KEY = 'AIzaSyCswdNc_cY9ISFJHfNgVBzQVBqU42XvXHM'; */
 
 
 function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
-    event.preventDefault();
-    let query = $(this).find('.js-search-input').val();
-    getDoctorList(query);
-    getHospitalList(query);
+    $('.js-search-form').submit(function(event) {
+        event.preventDefault();
+        let query = $(this).find('.js-search-input').val();
+        getHospitalList(query);
 
 
-  });
+    });
 }
 
-watchSubmit(); */
+watchSubmit();
