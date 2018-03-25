4da339cc080958b2c0ffc1bb0b85ee51b6d2355d
Ari Simon <arisimon2@gmail.com>
2018-03-25 13:17:23 -0700

    formatted hospital function request data
    formatted hospital function request data

--- index.js
+++ index.js
@@ -4,35 +4,55 @@ function bindEventListeners() {
     watchSubmit();
 }
 
-const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
+// const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
 
-/*function getHospitalList() {
-    $.ajax({
-        url: HOSPITAL_URL,
-        type: "GET",
-        dataType: "json",
-        data: {
-            "$limit": 15,
-        }
+// function getHospitalList() {
+//     $.ajax({
+//         url: HOSPITAL_URL,
+//         type: "GET",
+//         dataType: "json",
+//         data: {
+//             "$limit": 15,
+//         }
 
 
-    }).done(function(data) {
-        alert("Retrieved " + data.length + " records from the dataset!");
-        console.log(data);
-        $.each(data, function(index, value) {
-            $('.hospital-list').append(
-                `<div class="row listing-container">
-					  <div class="listing col-8" style="background-color:#aaa;">
-					    <h2>` + data[index].hospital_name + `</h2>
-					    <p>` + data[index].city + `</p>
-					  </div>`
-            )
+//     }).done(function(data) {
+//         alert("Retrieved " + data.length + " records from the dataset!");
+//         console.log(data);
+//         $.each(data, function(index, value) {
+//             $('.hospital-list').append(
+//                 `<div class="col-8" id="content" class="clearfix">
+//         <div id="userphoto"><img src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/128x128/hospital.png" alt="hospital-icon"></div>
+//           <h2>` + data[index].hospital_name + `</h2>     
+//         <section id="bio">
+//           <h4><span>Hospital Type: </span>` + data[index].hospital_ownership + `</h4>
+//                 <h4><span>Address: </span>` + data[index].address + ` ` + data[index].city + `, ` + data[index].state + ` ` + data[index].zip_code + `</h4>
+          
+//         </section>
+        
+//         <section id="detail">
+//           <p><i>How does this hospital compare to the national average?</i></p>
 
-        });
+//           <p class="detail"><span>Effectiveness of care: </span>` + data[index].effectiveness_of_care_national_comparison + `</p>
+          
+//           <p class="detail"><span>Patient experience: </span>` + data[index].patient_experience_national_comparison + `</p>
+          
+//           <p class="detail"><span>Mortality rate: </span>` + data[index].mortality_national_comparison + `</p>
 
-    });
+//           <p class="detail"><span>Safety of care: </span>` + data[index].safety_of_care_national_comparison + `</p>
+          
+//           <p class="detail"><span>Timeliness of care: </span>` + data[index].timeliness_of_care_national_comparison + `</p>
 
-}; */
+//         </section>
+//       </div>
+//   </div>`
+//             )
+
+//         });
+
+//     });
+
+// };
 
 const DOCTOR_URL = 'https://api.betterdoctor.com/2016-03-01/doctors';
 const API_KEY = '8ed32e7cd794252c93e1eaabd46b41d2';
@@ -52,73 +72,44 @@ function getDoctorList() {
     }).done(function(results) {
         let data = results.data;
         $.each(data, function(index, value) {
+            console.log('data successfully loaded', data);
             $('.hospital-list').append(
-                `<div id="w">
-	    <div id="content" class="clearfix">
+                `<div class="col-8" id="content" class="clearfix">
 	      <div id="userphoto"><img src="` + data[index].profile.image_url + `" alt="avatar"></div>
-	      	<h1>` + data[index].profile.first_name + ` ` + data[index].profile.last_name + ` ` + data[index].profile.title + `</h1>
-
-	      <nav id="profiletabs">
-	        <ul class="clearfix">
-	          <li><a href="#bio" class="sel">Bio</a></li>
-	          <li><a href="#settings">Contact Information</a></li>
-	        </ul>
-	      </nav>
-	      
+	      	<h2>` + data[index].profile.first_name + ` ` + data[index].profile.last_name + `, ` + data[index].profile.title + `</h2>     
 	      <section id="bio">
-	      	<h4><span>Specialties: </span>` + data[index].specialties.name +
-                `<p>` + data[index].profile.bio + `</p>
+	      	<h4><span>Specialties: </span>` + data[index].specialties[0].name +
+                `<p id="bio">` + data[index].profile.bio + `</p>
 	      </section>
 	      
-	      <section id="settings" class="hidden">
-	        <p>Contact Information is listed below:</p>
+	      <section id="details">
+	        <p><i>Contact Information is listed below:</i></p>
 
-	        <p class="setting"><span>Practice Name: </span>` + data[index].practices[0].name + `</p>
+	        <p class="detail"><span>Practice Name: </span>` + data[index].practices[0].name + `</p>
 	        
-	        <p class="setting"><span>Phone Number: </span>` + data[index].practices[0].phones[0].number + `</p>
+	        <p class="detail"><span>Phone Number: </span>` + data[index].practices[0].phones[0].number + `</p>
 	        
-	        <p class="setting"><span>Address: </span>` + data[index].practices[0].visit_address.street + ` ` + data[index].practices[0].visit_address.street2 + `<br>` +
+	        <p class="detail"><span>Address: </span>` + data[index].practices[0].visit_address.street + `, ` +
                 data[index].practices[0].visit_address.city + `, ` + data[index].practices[0].visit_address.state + ` ` + data[index].practices[0].visit_address.zip + `</p>
 
-	        <p class="setting"><span>Website: </span>` + data[index].practices.website + `</p>
+	        <p class="detail"><span>National Provider Identifier Standard (NPI) Number: </span>` + data[index].npi + `</p>
 	      </section>
 	    </div>
-  </div>
-}
-        });`
+  </div>`
+
             );
 
         });
     });
 }
 
-/* Controls functionality of Doctor's list response */
-function bioTabs() {
-    $(function() {
-        $('#profiletabs ul li a').on('click', function(e) {
-            e.preventDefault();
-            var newcontent = $(this).attr('href');
-
-            $('#profiletabs ul li a').removeClass('sel');
-            $(this).addClass('sel');
-
-            $('#content section').each(function() {
-                if (!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
-            });
-
-            $(newcontent).removeClass('hidden');
-        });
-    });
-}
 
 
 function watchSubmit() {
     $('.js-search-form').submit(function(event) {
         event.preventDefault();
-        let query = $(this).find('.js-search-input').val();
+        let query = $(this).find('.js-search-input')
         getDoctorList(query);
-
-
         $('.js-search-form').hide();
 
 
