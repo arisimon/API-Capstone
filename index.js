c5b9244177a748b8b34ab2378374cac918411984
Ari Simon <arisimon2@gmail.com>
2018-03-25 08:59:33 -0700

    upload before thinkful session
    all progress before meeting with mentor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function bindEventListeners() {
-	watchSubmit();
+    watchSubmit();
 }
 
 const HOSPITAL_URL = 'https://data.medicare.gov/resource/rbry-mqwu.json';
@@ -51,12 +51,13 @@ function getDoctorList() {
 
     }).done(function(results) {
         console.log(results);
-        $.each(results, function(i, value) {
-        	$('.hospital-list').append(
-        `<div id="w">
+    }).done(function(results) {
+        $.each(results, function(index, value) {
+            $('.hospital-list').append(
+                `<div id="w">
 	    <div id="content" class="clearfix">
-	      <div id="userphoto"><img src="` + results.data[i].profile.image_url + `" alt="avatar"></div>
-	      	<h1>` + results.data[i].profile.first_name + ` ` + results.data[i].profile.last_name + ` ` + results.data[i].profile.title + `</h1>
+	      <div id="userphoto"><img src="` + results.data[index].profile.image_url + `" alt="avatar"></div>
+	      	<h1>` + results.data[index].profile.first_name + ` ` + results.data[index].profile.last_name + ` ` + results.data[index].profile.title + `</h1>
 
 	      <nav id="profiletabs">
 	        <ul class="clearfix">
@@ -66,19 +67,19 @@ function getDoctorList() {
 	      </nav>
 	      
 	      <section id="bio">
-	      	<h4><span>Specialties: </span>` + data[i].specialties.name +
-	        `<p>` + results.data[i].profile.bio + `</p>
+	      	<h4><span>Specialties: </span>` + data[index].specialties.name +
+                `<p>` + results.data[index].profile.bio + `</p>
 	      </section>
 	      
 	      <section id="settings" class="hidden">
 	        <p>Contact Information is listed below:</p>
 
-	        <p class="setting"><span>Practice Name: </span>` + results.data[i].practices.name + `</p>
+	        <p class="setting"><span>Practice Name: </span>` + results.data[index].practices[0].name + `</p>
 	        
-	        <p class="setting"><span>Phone Number: </span>` + results.data[i].practices.phones[0].number `</p>
+	        <p class="setting"><span>Phone Number: </span>` + results.data[index].practices[0].phones[0].number `</p>
 	        
-	        <p class="setting"><span>Address: </span>` + results.data[i].practices.visit_address.street + ` ` + results.data[i].practices.visit_address.street2 + `<br>`
-	        + results.data[i].practices.visit_address.city + `, ` + results.data[i].practices.visit_address.state + ` ` + results.data[i].practices.visit_address.zip + `</p>
+	        <p class="setting"><span>Address: </span>` + results.data[index].practices[0].visit_address.street + ` ` + results.data[index].practices[0].visit_address.street2 + `<br>` +
+                results.data[index].practices[0].visit_address.city + `, ` + results.data[index].practices[0].visit_address.state + ` ` + results.data[index].practices[0].visit_address.zip + `</p>
 
 	        <p class="setting"><span>Website: </span>` + results.data[i].practices.website + `</p>
 	      </section>
@@ -86,29 +87,29 @@ function getDoctorList() {
   </div>
 }
         });`
-        		);
-        	
+            );
 
-    });
+        });
     });
 }
 
-function handleBio() {
-	$(function(){
-  $('#profiletabs ul li a').on('click', function(e){
-    e.preventDefault();
-    var newcontent = $(this).attr('href');
-    
-    $('#profiletabs ul li a').removeClass('sel');
-    $(this).addClass('sel');
-    
-    $('#content section').each(function(){
-      if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+/* Controls functionality of Doctor's list response */
+function bioTabs() {
+    $(function() {
+        $('#profiletabs ul li a').on('click', function(e) {
+            e.preventDefault();
+            var newcontent = $(this).attr('href');
+
+            $('#profiletabs ul li a').removeClass('sel');
+            $(this).addClass('sel');
+
+            $('#content section').each(function() {
+                if (!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+            });
+
+            $(newcontent).removeClass('hidden');
+        });
     });
-    
-    $(newcontent).removeClass('hidden');
-  });
-});
 }
 
 
@@ -116,7 +117,7 @@ function watchSubmit() {
     $('.js-search-form').submit(function(event) {
         event.preventDefault();
         let query = $(this).find('.js-search-input').val();
-        var why = $('input[name="why"]').val(); 
+        var why = $('input[name="why"]').val();
         getDoctorList(query);
 
 
